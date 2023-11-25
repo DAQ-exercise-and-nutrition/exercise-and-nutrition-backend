908176229835c157be02e2a00daccf81703dfe4b
Warat Poovorakit <warat.po@ku.th>
2023-11-25 14:33:24 +0700

    link the path from openapi to controller

--- stub/swagger_server/test/test_default_controller.py
+++ stub/swagger_server/test/test_default_controller.py
@@ -16,11 +16,15 @@ class TestDefaultController(BaseTestCase):
     def test_controller_get_exercises(self):
         """Test case for controller_get_exercises
 
-        Get a list of exercises
+        Get a list of all exercises
         """
+        query_string = [('target', 'target_example'),
+                        ('limit', 2),
+                        ('random', true)]
         response = self.client.open(
             '/exercise-and-nutrition-api/v3/exercises',
-            method='GET')
+            method='GET',
+            query_string=query_string)
         self.assert200(response,
                        'Response body is : ' + response.data.decode('utf-8'))
 
@@ -29,9 +33,12 @@ class TestDefaultController(BaseTestCase):
 
         Get exercises for a specific body part
         """
+        query_string = [('limit', 2),
+                        ('random', true)]
         response = self.client.open(
             '/exercise-and-nutrition-api/v3/exercises/body_part/{bodyPart}'.format(body_part='body_part_example'),
-            method='GET')
+            method='GET',
+            query_string=query_string)
         self.assert200(response,
                        'Response body is : ' + response.data.decode('utf-8'))
 
@@ -40,20 +47,28 @@ class TestDefaultController(BaseTestCase):
 
         Get exercises for a specific target
         """
+        query_string = [('limit', 2),
+                        ('random', true)]
         response = self.client.open(
             '/exercise-and-nutrition-api/v3/exercises/target/{target}'.format(target='target_example'),
-            method='GET')
+            method='GET',
+            query_string=query_string)
         self.assert200(response,
                        'Response body is : ' + response.data.decode('utf-8'))
 
     def test_controller_get_foods(self):
         """Test case for controller_get_foods
 
-        Get a list of foods
+        Get a list of all foods
         """
+        query_string = [('diet', 'diet_example'),
+                        ('meal_type', 'meal_type_example'),
+                        ('limit', 2),
+                        ('random', true)]
         response = self.client.open(
             '/exercise-and-nutrition-api/v3/foods',
-            method='GET')
+            method='GET',
+            query_string=query_string)
         self.assert200(response,
                        'Response body is : ' + response.data.decode('utf-8'))
 
@@ -62,9 +77,12 @@ class TestDefaultController(BaseTestCase):
 
         Get foods for a specific diet
         """
+        query_string = [('limit', 2),
+                        ('random', true)]
         response = self.client.open(
             '/exercise-and-nutrition-api/v3/foods/diet/{diet}'.format(diet='diet_example'),
-            method='GET')
+            method='GET',
+            query_string=query_string)
         self.assert200(response,
                        'Response body is : ' + response.data.decode('utf-8'))
 
@@ -73,9 +91,12 @@ class TestDefaultController(BaseTestCase):
 
         Get foods for a specific meal type
         """
+        query_string = [('limit', 2),
+                        ('random', true)]
         response = self.client.open(
             '/exercise-and-nutrition-api/v3/foods/meal_type/{mealType}'.format(meal_type='meal_type_example'),
-            method='GET')
+            method='GET',
+            query_string=query_string)
         self.assert200(response,
                        'Response body is : ' + response.data.decode('utf-8'))
 
