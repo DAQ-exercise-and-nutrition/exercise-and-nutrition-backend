3ceb120af4437da5d9a96782431a9e8e1ebe87d8
Warat Poovorakit <warat.po@ku.th>
2023-11-25 14:32:29 +0700

    implement swagger

--- stub/swagger_server/controllers/default_controller.py
+++ stub/swagger_server/controllers/default_controller.py
@@ -6,74 +6,104 @@ from swagger_server.models.food import Food  # noqa: E501
 from swagger_server import util
 
 
-def controller_get_exercises():  # noqa: E501
-    """Get a list of exercises
+def controller_get_exercises(target=None, limit=None, random=None):  # noqa: E501
+    """Get a list of all exercises
 
      # noqa: E501
 
+    :param target: 
+    :type target: str
+    :param limit: 
+    :type limit: int
+    :param random: 
+    :type random: bool
 
     :rtype: List[Exercise]
     """
     return 'do some magic!'
 
 
-def controller_get_exercises_by_body_part(body_part):  # noqa: E501
+def controller_get_exercises_by_body_part(body_part, limit=None, random=None):  # noqa: E501
     """Get exercises for a specific body part
 
      # noqa: E501
 
     :param body_part: 
     :type body_part: str
+    :param limit: 
+    :type limit: int
+    :param random: 
+    :type random: bool
 
     :rtype: List[Exercise]
     """
     return 'do some magic!'
 
 
-def controller_get_exercises_by_target(target):  # noqa: E501
+def controller_get_exercises_by_target(target, limit=None, random=None):  # noqa: E501
     """Get exercises for a specific target
 
      # noqa: E501
 
     :param target: 
     :type target: str
+    :param limit: 
+    :type limit: int
+    :param random: 
+    :type random: bool
 
     :rtype: List[Exercise]
     """
     return 'do some magic!'
 
 
-def controller_get_foods():  # noqa: E501
-    """Get a list of foods
+def controller_get_foods(diet=None, meal_type=None, limit=None, random=None):  # noqa: E501
+    """Get a list of all foods
 
      # noqa: E501
 
+    :param diet: 
+    :type diet: str
+    :param meal_type: 
+    :type meal_type: str
+    :param limit: 
+    :type limit: int
+    :param random: 
+    :type random: bool
 
     :rtype: List[Food]
     """
     return 'do some magic!'
 
 
-def controller_get_foods_by_diet(diet):  # noqa: E501
+def controller_get_foods_by_diet(diet, limit=None, random=None):  # noqa: E501
     """Get foods for a specific diet
 
      # noqa: E501
 
     :param diet: 
     :type diet: str
+    :param limit: 
+    :type limit: int
+    :param random: 
+    :type random: bool
 
     :rtype: List[Food]
     """
     return 'do some magic!'
 
 
-def controller_get_foods_by_meal_type(meal_type):  # noqa: E501
+def controller_get_foods_by_meal_type(meal_type, limit=None, random=None):  # noqa: E501
     """Get foods for a specific meal type
 
      # noqa: E501
 
     :param meal_type: 
     :type meal_type: str
+    :param limit: 
+    :type limit: int
+    :param random: 
+    :type random: bool
 
     :rtype: List[Food]
     """
