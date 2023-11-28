46cd5ff7fe2148b263d2a075553fbef8094858cc
Warat Poovorakit <warat.po@ku.th>
2023-11-28 23:08:16 +0700

    remove important information in config file

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 OPENAPI_STUB_DIR = 'stub'
-DB_HOST = "iot.cpe.ku.ac.th"
+DB_HOST = "YOUR NETWORK"
 
-DB_USER = "b6410545771"
-DB_PASSWD = "warat.po@ku.th"
+DB_USER = "YOUR USER ID"
+DB_PASSWD = "YOUR PASSWORD (EMAIL)"
 DB_NAME = "group12"
